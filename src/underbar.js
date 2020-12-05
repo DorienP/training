8d79d8c8b0889029136b0ce61cb3792906a55ce1
Dorien <dorien.pine@gmail.com>
2020-12-05 11:01:58 -0700

    Assignment Complete underbar part 1

--- src/underbar.js
+++ src/underbar.js
@@ -175,6 +175,20 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (iterator === undefined) {
+      iterator = _.identity();
+    };
+    if (accumulator === undefined) {
+      var accumulator = collection[0];
+      for (var i=1;i<collection.length;i++) {
+        accumulator = iterator(accumulator, collection[i], i, collection);
+      }
+    }else {
+      _.each(collection, function(value, index, collection) {
+       accumulator = iterator(accumulator, value, index, collection);
+      });
+    }
+    return accumulator;
   };
 
 
